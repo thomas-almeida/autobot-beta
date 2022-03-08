177ebdb8cc6ebd7b4e4bf27561c1a842f8c8ed13
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2022-03-07 21:19:21 -0300

    Novo index.js // bot entrando em voice channel

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-//conectando biblioteca Discord e criando client
-const Discord = require("discord.js")
+const Discord = require('discord.js')
 
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES"]
 })
 
-//Notificar status online!
+const TK = 'OTMyNDgxMjIyNTA3MTEwNDAw.YeTm0A.5_2vkfyvPNb9yjdT5S8qDqZ7c0M'
+const prefixo = "$"
+
 client.on("ready", () => {
-    console.log("Estou online ✔")
+    console.log('Estou Online')
 })
 
-//monitorarando mensagens dos usuarios
-client.on("messageCreate", (message) => {
+client.on("message", async(msg) => {
+    // filtro
+    if (!msg.guild) return
 
-    if (message.content == "Ola") {
-        console.log("Usuario mandou Ola")
-    } else {
-        console.log(message.content)
+    if (!msg.content.startsWith(prefixo)) return
+
+    // comandos
+    if (msg.content == prefixo + 'aoba') {
+        msg.channel.send('Bão ' + msg.author.username + '?')
     }
 
 })
 
-//comandos que o bot pode responder
-client.on("messageCreate", (message) => {
 
-    //Comando de Ping
-    if (message.content == "$ping") {
-        message.channel.send("Pong")
+//discord.js and client declaration
+const { joinVoiceChannel } = require('@discordjs/voice');
+client.on('messageCreate', message => {
+    if (message.content === prefixo + 'join') {
+        joinVoiceChannel({
+            channelId: message.member.voice.channel.id,
+            guildId: message.guild.id,
+            adapterCreator: message.guild.voiceAdapterCreator
+        })
     }
-
-    //evitar conflito entre bots e mensagens na DM
-    if (message.author.bot == true) return
-    if (message.channel.type == "dm") return
-
-
-
 })
 
-//login de Token do BOT
-client.login("OTMyNDgxMjIyNTA3MTEwNDAw.YeTm0A.iXT5N1d2hItuJgDLCVdt38ufMl8")
+client.login(TK)
